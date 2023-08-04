f93a39a4c9b244bff9f53da9f4e786937a396fdf
Douglas Wenzler Santos <douglas@MacBook-Air-de-Douglas.local>
2023-08-04 00:35:31 -0300

    First screen finished

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors')
 
 const Order = require("./models/order");
 const Plan = require("./models/plans");
@@ -11,6 +12,7 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 console.log("Running!")
 
